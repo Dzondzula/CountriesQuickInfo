8c468eb8220837c2e02997698a5a90396ae9a5ea
Dzondzula <www.nikolaandrijasevic@gmail.com>
2021-11-12 19:28:24 +0100

    Testing commit

--- CountriesFinal/CountriesFinal/ViewControllers/CountriesViewController.swift
+++ CountriesFinal/CountriesFinal/ViewControllers/CountriesViewController.swift
@@ -30,11 +30,11 @@ class CountriesViewController: UITableViewController,UISearchBarDelegate{
         //"ttp://api.countrylayer.com/v2/all?access_key=88e5f402e6864cc63ff8378ee100f32b"
     let url = URL(string:urlString)!
         //note that we’re assigning the return value of dataTask(with:completionHandler:) to the task constant
-        // completion handler is a bit more complicated. It’s a closure that’s executed when the request completes, so when a response has returned from the webserver. This can be any kind of response, including errors, timeouts, 404s, and actual JSON data.
+        // completion handler is a closure that’s executed when the request completes, so when a response has returned from the webserver. This can be any kind of response, including errors, timeouts, 404s, and actual JSON data.
         //closure has three parameters: the response Data object, a URLResponse object, and an Error object. All of these closure parameters are optionals, so they can be nil.
             DispatchQueue.global(qos: .userInitiated).async {
                 [self] in
-            
+            //Data tasks send and receive data with URLSessionDataTask, by using NSData objects. They’re the most common for webservice requests, for example when working with JSON.
                 session.dataTask(with: url) {[self]data,response,error in
             if let data = data {
                 if let decodedResponse = try?
